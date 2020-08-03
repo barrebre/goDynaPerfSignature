2b86d1a41e9d6c81fe39c662fdd2d120c9272aaa
Brett Barrett <barrebre@icloud.com>
2020-08-02 22:25:33 -0500

    Clarification in the datatypes package

--- datatypes/performancesignature.go
+++ datatypes/performancesignature.go
@@ -2,14 +2,6 @@ package datatypes
 
 //// Definitions
 
-// Metric defines a Dynatrace Service we'd like to investigate and how we'd like to validate it
-type Metric struct {
-	ID                string
-	RelativeThreshold float64
-	StaticThreshold   float64
-	ValidationMethod  string
-}
-
 // PerformanceSignature is a struct defining all of the parameters we need to calculate a performance signature
 type PerformanceSignature struct {
 	APIToken       string
